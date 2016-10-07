97207f63fae1b4abfd08b1345f19000c99fd23bf
Sven Tatter <sven31894@gmail.com>
2016-10-08 00:16:18 +0200

    add range() and delete()

--- index.ts
+++ index.ts
@@ -16,16 +16,18 @@ export interface EtcdOptions {
 }
 
 export class Etcd {
-	server: string;
+	servers: string[];
 	options: EtcdOptions;
 	credentials: any = grpc.credentials.createInsecure();
 	clients: any;
 
-	constructor(server: string = "localhost:2379", options: EtcdOptions = {}) {
-		this.server = server;
+	constructor(servers: string[] = [ "localhost:2379" ], options: EtcdOptions = {}) {
+		this.servers = servers;
+		if (this.servers.length > 1)
+			console.warn("etcd3: currently only the first server address is used");
 		this.options = options;
 		this.clients = {
-			KV: new etcdProto.etcdserverpb.KV(this.server, this.credentials)
+			KV: new etcdProto.etcdserverpb.KV(this.servers[0], this.credentials)
 		};
 	}
 
@@ -45,42 +47,73 @@ export class Etcd {
 			this.clients[client][method](arg, (err, response) => {
 				if (err) {
 					reject(new EtcdError(`etcd ${client}.${method} failed`, err));
-					return
+					return;
 				}
 
-				resolve(response)
+				resolve(response);
 			});
-		})
+		});
 	}
 
 	/**
 	 * blocking version of get()
 	 * @see {@link get}
 	 */
-	getSync(key: string) { return deasyncPromise(this.get(key)) }
+	getSync(key: string): string {
+		return deasyncPromise(this.get(key));
+	}
 	/**
 	 * get a key from etcd
 	 * @param key - key to get
 	 * @return value of the key
 	 */
-	async get(key: string) {
+	async get(key: string): Promise<string> {
 		return this.callClient("KV", "range", {
-			key: this.getBuffer(key)
+			key: new Buffer(key)
 		}).then((res) => {
 			if (res.kvs.length) {
-				return res.kvs[0].value.toString()
+				return res.kvs[0].value.toString();
 			} else {
 				// key not found
 				return null;
 			}
-		})
+		});
+	}
+
+	/**
+	 * blocking version of range()
+	 * @see {@link range}
+	 */
+	rangeSync(fromKey: string, toKey: string): any {
+		return deasyncPromise(this.range(fromKey, toKey));
+	}
+	/**
+	 * get a range of key from etcd
+	 * @param fromKey - key to start from
+	 * @param toKey - key to end on
+	 * @return value of the key
+	 */
+	async range(fromKey: string, toKey: string): Promise<any> {
+		return this.callClient("KV", "range", {
+			key: new Buffer(fromKey),
+			range_end: new Buffer(toKey),
+			limit: 500
+		}).then((res) => {
+			let result: any = {};
+			for (let kv of res.kvs) {
+				result[kv.key.toString()] = kv.value.toString();
+			}
+			return result;
+		});
 	}
 
 	/**
 	 * blocking version of set()
 	 * @see {@link set}
 	 */
-	setSync(key: string, value: any) { return deasyncPromise(this.set(key, value)) }
+	setSync(key: string, value: any): boolean {
+		return deasyncPromise(this.set(key, value));
+	}
 	/**
 	 * set a key/value to etcd.
 	 * * a `Buffer` is saved as-is
@@ -92,12 +125,33 @@ export class Etcd {
 	 * @param value - content to set
 	 * @return `true`
 	 */
-	async set(key: string, value: any) {
+	async set(key: string, value: any): Promise<boolean> {
 		return this.callClient("KV", "put", {
-			key: this.getBuffer(key),
+			key: new Buffer(key),
 			value: this.getBuffer(value)
 		}).then((res) => {
-			return true
-		})
+			return true;
+		});
+	}
+
+	/**
+	 * blocking version of delete()
+	 * @see {@link delete}
+	 */
+	deleteSync(key: string, keyTo?: string): number {
+		return deasyncPromise(this.delete(key, keyTo));
+	}
+	/**
+	 * delete a key from etcd.
+	 * @param key - etcd key to delete
+	 * @return number of keys deleted
+	 */
+	async delete(key: string, keyTo?: string): Promise<number> {
+		return this.callClient("KV", "deleteRange", {
+			key: new Buffer(key),
+			range_end: keyTo ? new Buffer(keyTo) : null
+		}).then((res) => {
+			return res.deleted;
+		});
 	}
 }
--- test/kv.ts
+++ test/kv.ts
@@ -3,11 +3,16 @@ import { Etcd } from "..";
 
 const etcd = new Etcd();
 
+test.before("clean etcd", (t) => {
+	let deletedKeys = etcd.deleteSync("test", "\0");
+	console.log(`cleaned ${deletedKeys} keys from etcd`);
+});
+
 let keyCounter = 0;
 test.beforeEach("create a random key", (t: any) => {
 	// for some reason the `t` isn't set to have a `context` in `beforeEach`
 	t.context.rkey = "test_" + keyCounter++;
-})
+});
 
 test("set() return true", async (t) => {
 	t.true(await etcd.set("testKey", 34));
@@ -31,6 +36,12 @@ test("set() store null as empty", async (t) => {
 	t.is(value, "");
 });
 
+test("set() store number as string", async (t) => {
+	await etcd.set(t.context.rkey, 34);
+	let value = await etcd.get(t.context.rkey);
+	t.is(value, "34");
+});
+
 test("set() store JSON for object", async (t) => {
 	let object = { test: 34 };
 	await etcd.set(t.context.rkey, object);
@@ -45,11 +56,37 @@ test("set() store JSON for array", async (t) => {
 	t.is(value, JSON.stringify(object));
 });
 
-/*test("get() return always string", async (t) => {
-	await etcd.set("number", 34);
-	t.is(typeof (await etcd.get("number")), "string");
-});*/
-
 test("get() return null for unset key", async (t) => {
 	t.is(await etcd.get(t.context.rkey), null);
+});
+
+test("range() return {} for unset keys", async (t) => {
+	let value = await etcd.range(t.context.rkey + "_1", t.context.rkey + "_9");
+	t.deepEqual(value, {});
+});
+
+test("range() returns keys", async (t) => {
+	const keys = {
+		[t.context.rkey + "_1"]: "value 1",
+		[t.context.rkey + "_3"]: "value 2",
+		[t.context.rkey + "_4"]: "value 3",
+		[t.context.rkey + "_7"]: "value 4",
+		[t.context.rkey + "_8"]: "value 5",
+	};
+	for (let kv in keys) {
+		await etcd.set(kv, keys[kv]);
+	}
+	let value = await etcd.range(t.context.rkey + "_1", t.context.rkey + "_9");
+	t.deepEqual(value, keys);
+});
+
+test("setSync()", (t) => {
+	let result = etcd.setSync(t.context.rkey, 34);
+	t.true(result);
+});
+
+test("getSync()", (t) => {
+	etcd.setSync(t.context.rkey, "getSync test");
+	let value = etcd.getSync(t.context.rkey);
+	t.is(value, "getSync test");
 });
